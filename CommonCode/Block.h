18cc5433511b07d0895153bfa2d302f5d8716ca5
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-06-07 18:27:40 +0200

    Moved Json parser to Block + updated dependencies

--- CommonCode/Block.h
+++ CommonCode/Block.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "float3.h"
+#include "HelperEnums.h"
 #include <algorithm>
 #include <vector>
 #include <string>
@@ -23,3 +24,12 @@ struct BlockType
     bool bOpaque;
     std::vector<BlockInfo> Blocks;
 };
+
+bool IsFaceHidden(const float3& blockPos, const float3& posToCompare);
+
+namespace BlockUtils
+{
+    void JsonToOBJ(const std::wstring& inputPath, const std::wstring& outputPath);
+}
+
+
